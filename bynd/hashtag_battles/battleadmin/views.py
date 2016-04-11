a3417bd0e1d654c4b48e0bebd2de062dae734379
torrange <tobiasjone@gmail.com>
2016-04-11 15:09:36 +0100

    building gui

--- bynd/hashtag_battles/battleadmin/views.py
+++ bynd/hashtag_battles/battleadmin/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.template import Template
 from django.shortcuts import render
+from battleadmin.models import Battle
 
 def index(request):
-    return render(request, 'battles.html')
+    battles = [b for b in Battle.objects.all()]
+    return render(request, 'battles.html', battles=battles)
