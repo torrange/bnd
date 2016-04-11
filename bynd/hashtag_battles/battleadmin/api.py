7917f4e9f6d61966c0f6c6f23b1dcfdaa1bef613
torrange <tobiasjone@gmail.com>
2016-04-11 13:08:22 +0100

    tidied JSON response attrs

--- bynd/hashtag_battles/battleadmin/api.py
+++ bynd/hashtag_battles/battleadmin/api.py
@@ -10,10 +10,10 @@ class BattleResource(ModelResource):
 
     def dehydrate(self, bundle):
         bundle.data['winner'] = bundle.obj.hwinr()
-        bundle.data['h1tag'] = bundle.obj.h1tag()
-        bundle.data['h1typ'] = bundle.obj.h1typ()
-        bundle.data['h2tag'] = bundle.obj.h2tag()
-        bundle.data['h2typ'] = bundle.obj.h2typ()
+        bundle.data['hashtag1'] = bundle.obj.h1tag()
+        bundle.data['hashtag1_typos'] = bundle.obj.h1typ()
+        bundle.data['hashtag2'] = bundle.obj.h2tag()
+        bundle.data['hashtag2_typos'] = bundle.obj.h2typ()
       
         return bundle
 
