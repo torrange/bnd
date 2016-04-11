5d8b284c062cab58701ee609f9cfa412ad956863
torrange <tobiasjone@gmail.com>
2016-04-11 14:57:52 +0100

    adding PureCSS front end

--- bynd/hashtag_battles/battleadmin/views.py
+++ bynd/hashtag_battles/battleadmin/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-
+from django.template import Template
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'battles.html')
