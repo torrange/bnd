582a82fb22813955526f99f20a2de239e48d649c
torrange <tobiasjone@gmail.com>
2016-04-12 02:58:29 +0100

    tidying

--- bynd/hashtag_battles/battleadmin/admin.py
+++ bynd/hashtag_battles/battleadmin/admin.py
@@ -1,17 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
-from tastypie.admin import ApiKeyInline
 from .models import Battle
 from .models import Hashtag
 
 
-class UserModelAdmin(UserAdmin):
-    inlines = UserAdmin.inlines + [ApiKeyInline]
-
-
-admin.site.unregister(User)
-admin.site.register(User, UserModelAdmin)
 admin.site.register(Battle)
 admin.site.register(Hashtag)
 
