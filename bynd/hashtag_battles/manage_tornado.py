92425e36a577db2168b49c0c2e9bcf5c612f57bd
torrange <tobiasjone@gmail.com>
2016-04-19 15:49:34 +0100

    cleaned test.py

--- bynd/hashtag_battles/manage_tornado.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os
-import sys
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hashtag_battles.settings")
-    from subprocess import call
-    call(["python2", "test.py"])
-
--- bynd/hashtag_battles/test.py
+++ bynd/hashtag_battles/test.py
@@ -8,37 +8,6 @@ import tornado.wsgi
 from battleadmin.models import Battle
 from battleadmin.models import Hashtag
 
-
-
-
-
-class BaseHandler(tornado.web.RequestHandler):
-    @property
-    def db(self):
-	return self.application.db
-
-#
-# Creates a new user, when you hit the url
-# http://localhost:8080/user/new?id=100&name=jane
-#
-
-class NewUserHandler(BaseHandler):
-    def get(self):
-	idstr = self.get_argument("id", 10)
-	id = int(idstr)
-	name = self.get_argument("name", "john")
-	sqlstr = 'insert into users (id,name) values (%d, "%s");' % (id,name) 
-	self.db.execute(sqlstr)
-    
-#
-# Get all users if you go to 
-# http://localhost:8080/   or
-# http://localhost:8080/user 
-#
-# Get a specific user (e.g. id=100) if you go to
-# http://localhost:8080/user?id=2
-#
-#
 class GetUserHandler(tornado.web.RequestHandler):
     def get(self):
 	battles = queryset = Battle.objects.all()
@@ -47,7 +16,7 @@ class GetUserHandler(tornado.web.RequestHandler):
 
 class Application(tornado.web.Application):
     def __init__(self):
-	handlers =  [ (r"/", GetUserHandler), (r"/user/new", NewUserHandler), (r"/user", GetUserHandler) ]
+	handlers =  [ (r"/", GetUserHandler),]
 	settings = {}
 	tornado.web.Application.__init__(self, handlers, **settings)
 
