f893397b5307e07f9caf544cb85ad2d61f89fc37
torrange <tobiasjone@gmail.com>
2016-04-19 14:15:07 +0100

    wip

--- bynd/hashtag_battles/manage_tornado.py
+++ bynd/hashtag_battles/manage_tornado.py
@@ -4,7 +4,6 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hashtag_battles.settings")
+    from subprocess import call
+    call(["python2", "test.py"])
 
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
