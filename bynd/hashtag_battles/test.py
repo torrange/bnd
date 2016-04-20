ee76e7553449201ddcf90b96770871652dfddd01
torrange <tobiasjone@gmail.com>
2016-04-20 01:25:57 +0100

    added twitter api to test.py tornado handler

--- bynd/hashtag_battles/test.py
+++ bynd/hashtag_battles/test.py
@@ -9,9 +9,9 @@ from battleadmin.models import Battle
 from battleadmin.models import Hashtag
 from twitter import *
 
-auth = OAuth("722577635939889152-R5qyydnehjWRd9ZFwmK3K5PZEjg6MC3",\ 
-            "Ig4EC87EzQxilIhedGUKM7jPZleUmJLFc0bHnJuuAjnfK",\
-	    "pPCXeAvd9wv5WP9WlA1h52fWd",\
+auth = OAuth("722577635939889152-R5qyydnehjWRd9ZFwmK3K5PZEjg6MC3",
+	    "Ig4EC87EzQxilIhedGUKM7jPZleUmJLFc0bHnJuuAjnfK",
+	    "pPCXeAvd9wv5WP9WlA1h52fWd",
 	    "62us2GYwyUniV9TtDavbTdlCjS3nw22N8bCiN41UwrnDGEc7iX")
 t = Twitter(auth=auth)
 
