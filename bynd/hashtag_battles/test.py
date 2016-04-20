b5317048313f2bfac45a9e87cc817291722b854d
torrange <tobiasjone@gmail.com>
2016-04-20 01:23:37 +0100

    added twitter api to test.py tornado handler

--- bynd/hashtag_battles/test.py
+++ bynd/hashtag_battles/test.py
@@ -7,11 +7,19 @@ import tornado.web
 import tornado.wsgi
 from battleadmin.models import Battle
 from battleadmin.models import Hashtag
+from twitter import *
+
+auth = OAuth("722577635939889152-R5qyydnehjWRd9ZFwmK3K5PZEjg6MC3",\ 
+            "Ig4EC87EzQxilIhedGUKM7jPZleUmJLFc0bHnJuuAjnfK",\
+	    "pPCXeAvd9wv5WP9WlA1h52fWd",\
+	    "62us2GYwyUniV9TtDavbTdlCjS3nw22N8bCiN41UwrnDGEc7iX")
+t = Twitter(auth=auth)
 
 class GetUserHandler(tornado.web.RequestHandler):
     def get(self):
 	battles = queryset = Battle.objects.all()
         hashtags = Hashtag.objects.all()
+	print hashtags
 	self.write({"response":"OK"})
 
 class Application(tornado.web.Application):
@@ -22,6 +30,11 @@ class Application(tornado.web.Application):
 
 
 def main():
+    auth = OAuth("722577635939889152-R5qyydnehjWRd9ZFwmK3K5PZEjg6MC3",\ 
+	"Ig4EC87EzQxilIhedGUKM7jPZleUmJLFc0bHnJuuAjnfK",\
+	"pPCXeAvd9wv5WP9WlA1h52fWd",\
+	"62us2GYwyUniV9TtDavbTdlCjS3nw22N8bCiN41UwrnDGEc7iX")
+    t = Twitter(auth=auth)
     settings = {} 
     http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(8080)
