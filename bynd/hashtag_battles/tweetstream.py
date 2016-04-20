1d9c56a3c9ac983bcddb02d599e37b8d8cc7e94e
torrange <tobiasjone@gmail.com>
2016-04-21 00:19:00 +0100

    added bayespell and tweetstream

---
+++ bynd/hashtag_battles/tweetstream.py
@@ -0,0 +1,35 @@
+from battleadmin.models import Battle
+from battleadmin.models import Hashtag
+from twitter import *
+
+
+class GetUserHandler(tornado.web.RequestHandler):
+    def get(self):
+	battles = queryset = Battle.objects.all()
+        hashtags = Hashtag.objects.all()
+	print hashtags
+	self.write({"response":"OK"})
+
+class Application(tornado.web.Application):
+    def __init__(self):
+	handlers =  [ (r"/", GetUserHandler),]
+	settings = {}
+	tornado.web.Application.__init__(self, handlers, **settings)
+
+
+def main():
+    auth = OAuth("722577635939889152-R5qyydnehjWRd9ZFwmK3K5PZEjg6MC3", 
+	"Ig4EC87EzQxilIhedGUKM7jPZleUmJLFc0bHnJuuAjnfK",
+	"pPCXeAvd9wv5WP9WlA1h52fWd",
+	"62us2GYwyUniV9TtDavbTdlCjS3nw22N8bCiN41UwrnDGEc7iX")
+    t = Twitter(auth=auth)
+    settings = {} 
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(8080)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+main()
+
+
+
