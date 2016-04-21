049f331b7b6cfa94b72beba8f1bf4bfe0f0a03c8
torrange <tobiasjone@gmail.com>
2016-04-21 01:16:42 +0100

    modified tweetstream.py

--- bynd/hashtag_battles/tweetstream.py
+++ bynd/hashtag_battles/tweetstream.py
@@ -1,35 +1,17 @@
 from battleadmin.models import Battle
 from battleadmin.models import Hashtag
 from twitter import *
+from bayespell import *
 
 
-class GetUserHandler(tornado.web.RequestHandler):
-    def get(self):
-	battles = queryset = Battle.objects.all()
-        hashtags = Hashtag.objects.all()
-	print hashtags
-	self.write({"response":"OK"})
+battles = queryset = Battle.objects.all()
+hashtags = Hashtag.objects.all()
+print hashtags
 
-class Application(tornado.web.Application):
-    def __init__(self):
-	handlers =  [ (r"/", GetUserHandler),]
-	settings = {}
-	tornado.web.Application.__init__(self, handlers, **settings)
+auth = OAuth("722577635939889152-R5qyydnehjWRd9ZFwmK3K5PZEjg6MC3", 
+    "Ig4EC87EzQxilIhedGUKM7jPZleUmJLFc0bHnJuuAjnfK",
+    "pPCXeAvd9wv5WP9WlA1h52fWd",
+    "62us2GYwyUniV9TtDavbTdlCjS3nw22N8bCiN41UwrnDGEc7iX")
 
-
-def main():
-    auth = OAuth("722577635939889152-R5qyydnehjWRd9ZFwmK3K5PZEjg6MC3", 
-	"Ig4EC87EzQxilIhedGUKM7jPZleUmJLFc0bHnJuuAjnfK",
-	"pPCXeAvd9wv5WP9WlA1h52fWd",
-	"62us2GYwyUniV9TtDavbTdlCjS3nw22N8bCiN41UwrnDGEc7iX")
-    t = Twitter(auth=auth)
-    settings = {} 
-    http_server = tornado.httpserver.HTTPServer(Application())
-    http_server.listen(8080)
-    tornado.ioloop.IOLoop.instance().start()
-
-
-main()
-
-
+t = Twitter(auth=auth)
 
