6f694f4aab71e0922087033fc6cb0d28122df1fc
torrange <tobiasjone@gmail.com>
2016-04-21 03:19:08 +0100

    working cython spellchecker

--- bynd/hashtag_battles/tweetstream.py
+++ bynd/hashtag_battles/tweetstream.py
@@ -1,12 +1,12 @@
-from battleadmin.models import Battle
-from battleadmin.models import Hashtag
+#from battleadmin.models import Battle
+#from battleadmin.models import Hashtag
 from twitter import *
 from bayespell import *
 
 
-battles = queryset = Battle.objects.all()
-hashtags = Hashtag.objects.all()
-print hashtags
+#battles = queryset = Battle.objects.all()
+#hashtags = Hashtag.objects.all()
+#print hashtags
 
 auth = OAuth("722577635939889152-R5qyydnehjWRd9ZFwmK3K5PZEjg6MC3", 
     "Ig4EC87EzQxilIhedGUKM7jPZleUmJLFc0bHnJuuAjnfK",
