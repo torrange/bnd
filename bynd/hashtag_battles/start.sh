f20c690fa15e5ec49f606958b299b7fe1b9af91a
torrange <tobiasjone@gmail.com>
2016-04-23 12:50:13 +0100

    simpler threading + ipc w/zmq + network failure recovery

--- bynd/hashtag_battles/start.sh
+++ bynd/hashtag_battles/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python manage.py runserver 0.0.0.0:8000 > /dev/null 2<&1 & \
-python manage.py shell < tweetstream.py
+python manage.py shell < tweettest.py
--- bynd/hashtag_battles/tweettest.py
+++ bynd/hashtag_battles/tweettest.py
@@ -1,5 +1,5 @@
-#from tornado.concurrent import futures
-#from battleadmin.models import Hashtag
+from tornado.concurrent import futures
+from battleadmin.models import Hashtag
 from twitter import *
 import time
 import bayespell
@@ -7,9 +7,7 @@ import zmq
 import thread
 
 
-def hashtag_stream(hashtags):
-    #hashtag = str(hashtag)
-    #hashtag_object = Hashtag.objects.get(tag=hashtag)
+def hashtag_stream(hashtags, auth):
     print "listening for %s" % ",".join(hashtags)
     twitter_stream = TwitterStream(auth=auth)
     iterator = twitter_stream.statuses.filter(track=",".join(hashtags))
@@ -17,7 +15,6 @@ def hashtag_stream(hashtags):
         print "started thread worker for %s" % ",".join(hashtags)
         for tweet in iterator:
             try: 
-                #print tweet.keys()
                 hashtag = "#"
                 try:
                     for htag in hashtags:
@@ -25,46 +22,31 @@ def hashtag_stream(hashtags):
                             hashtag = str(htag)
                     err_count = bayespell.errors(tweet["text"])
                     print hashtag, err_count
-                    #hashtag_object = Hashtag.objects.get(tag=hashtag)
-                    #hashtag_object.typos = hashtag_object.typos + err_count
-                    #hashtag_object.save()
+                    hashtag_object = Hashtag.objects.get(tag=hashtag)
+                    hashtag_object.typos = hashtag_object.typos + err_count
+                    hashtag_object.save()
                     
                 except:
                     pass
-
                 continue
             except:
-                print "raise!"
                 time.sleep(3)
                 twitter_stream = TwitterStream(auth=auth)
                 iterator = twitter_stream.statuses.filter(track=",".join(hashtags))
                 continue
 
-                
 
-auth = OAuth("722577635939889152-R5qyydnehjWRd9ZFwmK3K5PZEjg6MC3",
+
+def main():
+    auth = OAuth("722577635939889152-R5qyydnehjWRd9ZFwmK3K5PZEjg6MC3",
              "Ig4EC87EzQxilIhedGUKM7jPZleUmJLFc0bHnJuuAjnfK",
              "pPCXeAvd9wv5WP9WlA1h52fWd",
              "62us2GYwyUniV9TtDavbTdlCjS3nw22N8bCiN41UwrnDGEc7iX")
-
-
-
-def makejob(hashtags):
-    print "creating job for: %s" % hashtags
-    x = thread.start_new_thread(hashtag_stream, (hashtags,))
-    thread_queue.append(x)
-
-
-                
-def main(makejob):
     thread_queue = []
-    #hashtags = [str(h.tag) for h in Hashtag.objects.all()]
-    hashtags = ["#Shakespeare400", "#StGeorgesDay"]
-    #makejob(hashtags)
+    hashtags = [str(h.tag) for h in Hashtag.objects.all()]
     print "creating thread for: %s" % hashtags
-    x = thread.start_new_thread(hashtag_stream, (hashtags,))
+    x = thread.start_new_thread(hashtag_stream, (hashtags, auth))
     thread_queue.append(x)
-    
     ctx = zmq.Context()
     socket = ctx.socket(zmq.SUB)
     socket.bind("tcp://0.0.0.0:6000")
@@ -76,11 +58,10 @@ def main(makejob):
         if msg in hashtags:
             print "%s updated" % msg
         else:
-            print "%s not in thread_queue" % msg
+            print "%s created" % msg
             hashtags.append(msg)
-            x = thread.start_new_thread(hashtag_stream, (hashtags,))
+            x = thread.start_new_thread(hashtag_stream, (hashtags, auth))
             thread_queue.append(x)
-            #makejob(msg)
         continue
 
-main(makejob)
+main()
